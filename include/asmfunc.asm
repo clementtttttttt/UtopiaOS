ee1c348e254992f899236aa714547c2e6a6a7dfe
BuildTools <unconfigured@null.spigotmc.org>
2020-05-27 23:51:49 +0800

    alpha 1.7: replaced the ugly font.

--- include/asmfunc.asm
+++ include/asmfunc.asm
@@ -1,7 +1,8 @@
 global asm_out
-
+global vbe_confirm
 ;extern void asm_out(char io_port,char io_data)
 asm_out:
 MOV DX,[ESP+4]
 MOV EAX,[ESP+8]
 OUT DX,EAX
+
--- kernel.c
+++ kernel.c
@@ -6,19 +6,38 @@
 
 #include "include/asmfunc.h"
  #include "include/font.h"
+int *resolution_ptr;
+  
+
+void init_vbe(){
+    regs16_t vberegister;
+  vberegister.bx=0x4107;
+  vberegister.ax=0x4f02;
+  int32(0x10,&vberegister);
+  if (vberegister.ax!=0x004f){
+    return;
+  }else
+  
+  *resolution_ptr=1280;
+}
 void init_vga(){
-    regs16_t vgaregister;
+  regs16_t vgaregister;
   vgaregister.ax=0x0013;
   int32(0x10,&vgaregister);
+  *resolution_ptr=320;
 }
-
 
 
 void kernel_main(void) 
 {
-  init_vga();
+  resolution_ptr=(int*)0x2ff0;
+
+  init_vbe ();
+
 
   printstring("Kernel sucessfully loaded.");
+  newline();
+  printstring("newline test.");
   
 
 
